cf2b6f032d1eafd565ca7172c5340c1858bf882a
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2022-12-04 22:10:41 +0100

    new one
    better one

--- binaryCalc.py
+++ binaryCalc.py
@@ -1,86 +1,144 @@
-#!/usr/bin/env python3
-#/bin/bash
 import sys
-summands = []
-operation = "+"
 
-def introduction():
-    print("Dieser Algorithmus addiert, subtrahiert, multipliziert und dividiert alle moeglichen binearen Werte miteinander")
-    print("Dabei gelten folgende Regeln:")
-    print("-----------------------------------------------------")
-    print("1. Leerzeichen, Sonderzeichen oder Buchstaben sind nicht erlaubt")
-    print("2. Die binearen Werte sind ohne Komma, Punkt oder Semikolon einzugeben. Zum Beispiel: 1010")
-    print("3. Die Operatoren sind in Form von \"+\", \"-\", \"*\", \"/\" einzugeben")
-    print("4. Alle Werte müssen die gleiche Länge besitzen")
-    print("-----------------------------------------------------\n")
-    
+class dataPreperation:
 
-def addition(summand1, summand2):
-    result = []
-    summand = summand.remove("p")
-    for i in range(len(summand1)):
-        if(summand1[i] == 1 and summand2[i] == 1):
-               rusult[i] = 1
-        else:
-            result[i] = 0
-    return result
+    def binaryToDecimal(self,value):
+        decimal = 0
+        sortedList = []
+        for x in range(len(value)-1,-1,-1):
+            sortedList.append(value[x])
+        for x in range(len(value)):
+            if(value[x]==1):
+                decimal = decimal + 2**x
+        return decimal
 
-def substraction(summand1, summand2):
-    result[]
-    for i in range(summand2):
-        if(summand2(i) == 1):
-            summand2(i) == 0)
-        if(summand2(i) == 0):
-            summand2(i) == 1
-    for i in range(len(summand1)):
-        if(summand1[i] == 1 and summand2[i] == 1):
-               rusult[i] = 1
-        else:
-            result[i] = 0
-
-    return result
-
-def multiplicate(factor1, factor2):
-    factor1.add(0)
-    factor2 = [0,factor2]
-    result = []
-    for i in range(len(factor1)):
-        if(factor1[i] == 1 and factor2[i] == 1):
-            result[i] = 0
-            result[i+1] = 1
-        if(factor1[i] == 0 and factor2[i] == 0):
-            result[i] = 0
-        if(factor1[i] == 1 Xor factor2[i] == 1):
-            result[i] = 1
-    return result
-
-def division(divisor1, divisor2):
-    
-
-def prepareInput(answer):
-    result = []
-    answer = answer.replace("+","p|") # give signal to add
-    answer = answer.replace("-","m|") # give signal to   subtract
-    answer = answer.replace("*","t|") # give signal to multiplicate
-    answer = answer.replace("/","d|") # give signal to divide
-    numbers = numbers.split("|")
-    
-    for i in numbers:
-        number = i.split("")
-        
-        try:
-            result[i] = int(number[i]) #must consider, that the operant-extension is not int
-        except NameError:
-            if(number[i] == "p" or number[i] == "m" or number[i] == "m" or number[i] == "d"):
-                result[i] = number[i]
+    def obtainValues(self):
+        values = []
+        operants = []
+        counter = 0
+        print("Enter the operants in the following. Notice to take the operator before the operant!")
+        while (True):
+            element = input("operant " + str((counter + 1)) + ":")
+            if (element == "exit"):
+                break
+            if (element[0] == "+" or element[0] == "-" or element[0] == "*" or element[0] == "/"):
+                operants.append(element[0])
+                element = element.replace("+","")
+                element = element.replace("-","")
+                element = element.replace("*","")
+                element = element.replace("/","")
+            print(element)
+            if (element == "exit"):
+                break
             else:
-                print("Du Idiot hast Buchstaben, Leerzeichen oder Sonderzeichen verwendet! Nochmal...")
-                Sys.exit()
-    return 
-            
-        
-introduction()
+                values.append(self.convertToByte(element))
+                counter = counter + 1
 
-while(True):
-    summands = prepareInput(input("Gib den vollsteandigen Term ein:"))
-    
+        for x in values:
+            x = self.extendZero(x)
+
+        return values, operants
+
+    def extendZero(self,list):
+        return list.insert(0,0)
+
+    def convertToByte(self,string):
+        resultList = []
+        for i in range(len(string)):
+            try:
+                resultList.append(int(string[i]))
+            except BaseException:
+                print("Error occoured: The input \""+string[i]+ "\" is no suitable value!")
+                sys.exit()
+        return  resultList
+
+class calculation:
+    prepare = dataPreperation
+    operant1 = 0
+    operant2 = 0
+
+    def search(self, operant1, operant2, operation):
+        print(operation)
+        if(operation == "+"):
+            print("took +")
+            return self.addition(operant1, operant2)
+        elif(operation == "-"):
+            return self.substraction(operant1, operant2)
+        elif(operation == "*"):
+            print("took *")
+            return self.multiplicationate(operant1, operant2)
+        elif(operation == "/"):
+            print("took /")
+            return self.division(operant1, operant2)
+        else:
+            print("Unhandable operant used. Restart algorithm!")
+            sys.exit()
+
+    def addition(self,operant1, operant2):
+        result = [0]*len(operant1)
+        transfer = 0
+        for x in range(len(operant1)-1,-1,-1):
+            if(operant1[x] == 1 and operant2[x] == 1):
+                result[x] = 0+ transfer
+                if(transfer == 0):
+                    transfer = 1
+            else:
+                result[x] = operant1[x]+operant2[x]+transfer
+                if(transfer + operant2[x] + operant1[x] == 2):
+                    result[x] == 0
+                else:
+                    transfer = 0
+
+        return result
+
+    def substraction(self, operant1, operant2):
+        if(self.prepare.binaryToDecimal(self.prepare, operant1) < self.prepare.binaryToDecimal(self.prepare,operant2)):
+            #build second complement
+            for x in range(len(operant2)):
+                if(operant2[x] == 1):
+                    operant2[x] = 0
+                else: operant2[x] = 1
+                plusOne = [0]*len(operant1)
+                plusOne[len(operant1)] = 1
+                operant2 = calculation.addition(operant1, plusOne)
+                return calculation.addition(operant1, operant2)
+        else:
+            transfer2 = 0
+            result = [0]*len(operant1)
+            for x in range(len(operant1)-1,-1,-1):
+                result[x],transfer1 = calculation.substractionCalcSystem(self,operant1[x], operant2[x])
+                result[x],z = calculation.substractionCalcSystem(self, result[x],transfer2)
+                transfer2 = transfer1
+            return result
+
+    def substractionCalcSystem(self, operant1, operant2):
+        if(operant1 == 1 and operant2 == 1):
+            return 0,0
+        elif(operant1 == 1 and operant2 == 0):
+            return 1,0
+        elif(operant1 == 0 and operant2 == 1):
+            return 1,1
+        elif(operant1 == 0 and operant2 == 0):
+            return 0,0
+
+    def multiplication(self):
+        pass
+    def division(self):
+        pass
+
+class Main:
+
+    prepare = dataPreperation()
+    calc = calculation()
+    global values #extends all operants
+    global operants # extends all operations for calculating
+
+    # structure
+    values, operants = prepare.obtainValues()
+
+    # adds to the first positive value a 0, so that the + can stay
+    if (operants[0] == "+"):
+        toInsert = [0]*len(values[0])
+        values.insert(0,toInsert)
+
+    print(calc.substraction([0,1,1,0,0,1,1],[0,1,0,1,1,0,1]))
